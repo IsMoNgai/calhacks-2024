eb623b80cb93da330d62d6cd4f24e779b6d07f5a
IsMoNgai <mongaiwaiyin@gmail.com>
2024-06-23 03:36:13 -0700

    roadmap

--- app/dashboard/roadmap/page.jsx
+++ app/dashboard/roadmap/page.jsx
@@ -1,18 +1,61 @@
-import React from 'react'
+'use client'
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import Task from '../../../components/task';
+import styles from "../../styles/Dashboard.roadmap.css";
 
-const page = () => {
+const Page = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const elH = document.querySelectorAll(".timeline li > div");
+    setEqualHeights(elH);
+  }, []);
+
+  const setEqualHeights = (el) => {
+    let counter = 0;
+    for (let i = 0; i < el.length; i++) {
+      const singleHeight = el[i].offsetHeight;
+      if (counter < singleHeight) {
+        counter = singleHeight;
+      }
+    }
+
+    for (let i = 0; i < el.length; i++) {
+      el[i].style.height = `${counter}px`;
+    }
+  };
+
+  const tasks = [
+    { id: 1, time: '1934', title: 'Title 1', description: 'Description 1' },
+    { id: 2, time: '1937', title: 'Title 2', description: 'Description 2' },
+    { id: 3, time: '1940', title: 'Title 3', description: 'Description 3' },
+  ];
+
   return (
     <div>
-        <div className="checkpoint">
-            <div>
-                <h2>title</h2>
-                <p>
-                    fasoidjfoiej foiasjfd oijsdoaif jsadoifdj soidafdj oidasjf oisdjafoi jsdaoidfj oiasdjf oisdajfoi djasdoifj oiasdjdf oiasjdoifja soidjoi
-                </p>
-            </div>
-        </div>
+      <div className="timeline-wrapper">
+        <section className="timeline">
+          <div className="info">
+          <button className="back-button" onClick={() => router.back()}>&larr;</button>
+            <img width="50" height="50" src="https://assets.codepen.io/210284/face.svg" alt="" />
+            <h2>Task Timeline</h2>
+            <p>You can see the entire timeline for the tasks you need to complete, along with their due dates and detailed descriptions.</p>
+          </div>
+          <ol>
+            {tasks.map(task => (
+              <Task
+                key={task.id}
+                time={task.time}
+                title={task.title}
+                description={task.description}
+              />
+            ))}
+          </ol>
+        </section>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
--- components/task.jsx
+++ components/task.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
 
-const task = () => {
+const Task = ({ time, title, description }) => {
+  const taskStyle = {
+    'background': 'rgba(168, 67, 220, 0.04)',
+    'border-radius': '16px',
+    'box-shadow': '0 4px 30px rgba(0, 0, 0, 0.1)',
+    'backdrop-filter': 'blur(11.6px)',
+    '-webkit-backdrop-filter': 'blur(11.6px)',
+    'border': '1px solid rgba(168, 67, 220, 0.71)'
+  };
+
   return (
-    <div>task</div>
-  )
-}
+    <li>
+      <div style={taskStyle}>
+        <time>{time}</time>
+        <h5>{title}</h5>
+        <p>{description}</p>
+      </div>
+    </li>
+  );
+};
 
-export default task
+export default Task;
